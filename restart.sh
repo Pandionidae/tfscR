7499060464175eee3a25684129f5bec0bb6d2ef2
Artur <arthur.escada.20@gmail.com>
2022-12-17 23:27:43 +0200

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,7 +1,17 @@
+rest37523563=0
 while true
 do
+
+if [ $rest37523563 -gt 10 ]
+then
+tmux kill-session -t tfsc
+sleep 5
+rest37523563=0
+printf "restart"
+printf "\n"
+fi
        tmux new-session -d -s tfsc 'cd $HOME/tfsc/ && $HOME/tfsc/tfsc -m'
-       
+       rest37523563=$((rest37523563 + 1))
         printf "sleep"
         for((sec=0; sec<60; sec++))
         do
@@ -9,4 +19,5 @@ do
                 sleep 1
         done
         printf "\n"
+        
 done
