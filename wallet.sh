b79d387d83cf190b5e09d93e0d1f7261e1c098c7
Artur <arthur.escada.20@gmail.com>
2022-12-29 21:43:01 +0200

    Update wallet.sh

--- wallet.sh
+++ wallet.sh
@@ -1,3 +1,13 @@
  tmux attach -t tfsc
 sleep 5
  printf "7"
+  printf "\n"
+fi
+        for((sec=0; sec<10; sec++))
+        do
+
+                sleep 10
+        done
+        printf "\n"
+        
+done
