3d03621d6fe57ec88a97a94b7e601b2197ecb28d
Artur <arthur.escada.20@gmail.com>
2022-12-29 21:57:13 +0200

    Update wallet.sh

--- wallet.sh
+++ wallet.sh
@@ -1,16 +1,1 @@
-cd $HOME/tfsc/ && $HOME/tfsc/tfsc -m
-printf "."
-sleep 10
-tmux attach -t tfsc
-sleep 105
- printf "7"
-  printf "\n"
-fi
-        for((sec=0; sec<1000; sec++))
-        do
-  printf "."
-                sleep 1
-        done
-        printf "\n"
-        
-done
+
