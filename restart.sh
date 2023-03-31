81fcf8cb4a37125606c2d630821ecfa75c9a107a
Artur <arthur.escada.20@gmail.com>
2023-03-31 14:19:16 +0300

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -10,7 +10,7 @@ rest37523563=0
 printf "restart"
 printf "\n"
 fi
-       tmux new-session -d -s tfsc 'cd $HOME/tfsc/ && $HOME/tfsc/tfsc -m'
+       tmux new-session -d -s tfsc 'cd $HOME/tfsc/ && $HOME/tfsc/tfs_v0.21.0_b011fcd_devnet -m'
        rest37523563=$((rest37523563 + 1))
         printf "sleep " 
         printf $rest37523563
