2ac6c50a2813bc4e2169b947a2d45568e3cd3558
Artur <arthur.escada.20@gmail.com>
2023-03-31 14:20:34 +0300

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -2,9 +2,9 @@ rest37523563=1
 while true
 do
 
-if test $rest37523563 -gt 360
+if test $rest37523563 -gt 720
 then
-tmux kill-session -t tfsc
+tmux kill-session -t tfs_v0.21.0_b011fcd_devnet
 sleep 5
 rest37523563=0
 printf "restart"
