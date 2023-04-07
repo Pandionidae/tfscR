8956ac14ddb8a4847a0a66d280e0f94e3b27ad3e
Artur <arthur.escada.20@gmail.com>
2023-04-07 18:25:32 +0300

    Update CeremonyS.sh

--- CeremonyS.sh
+++ CeremonyS.sh
@@ -2,5 +2,5 @@ printf "START\n"
 docker-compose -f $HOME/docker-compose.yaml run --rm --entrypoint "./bin/run ceremony" ironfish
 sleep 6
 printf "\n"
-printf "\n"'
+printf "1\n"'
 sleep 500000000000000
