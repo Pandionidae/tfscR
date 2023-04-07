3f052729cbdcb003a205951d06c6a0d06ccceea5
Artur <arthur.escada.20@gmail.com>
2023-04-07 18:14:36 +0300

    Update Ceremony.sh

--- Ceremony.sh
+++ Ceremony.sh
@@ -1,2 +1,4 @@
-tmux new-session -d -s ironfish 'docker-compose -f $HOME/docker-compose.yaml run --rm --entrypoint "./bin/run ceremony" ironfish && printf "\n" && printf "\n"'
-tmux attach -t ironfish
+docker-compose -f $HOME/docker-compose.yaml run --rm --entrypoint "./bin/run ceremony" ironfish
+printf "\n"
+printf "\n"'
+
