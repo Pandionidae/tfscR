14dc7ec2e09040ac19462f1f2b98566e60980adb
Artur <arthur.escada.20@gmail.com>
2023-04-07 19:05:03 +0300

    Delete CeremonyS.sh

--- CeremonyS.sh
+++
@@ -1,6 +0,0 @@
-printf "START\n"
-docker-compose -f $HOME/docker-compose.yaml run --rm --entrypoint "./bin/run ceremony && printf '%s\n'" ironfish 
-printf "START\n"
-printf "%s\n"
-printf "1\n"'
-sleep 500000000000000
