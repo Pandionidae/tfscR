03f37137395fabbd99835e1562f1d292b521673a
Artur <arthur.escada.20@gmail.com>
2023-04-30 20:10:50 +0300

    Create rest.sh

---
+++ rest.sh
@@ -0,0 +1,24 @@
+rest37523563=1
+while true
+do
+
+if test $rest37523563 -gt 720
+then
+tmux kill-session -t tfs_v0.22.1_fffda3e_devnet
+sleep 5
+rest37523563=0
+printf "restart"
+printf "\n"
+fi
+       tmux new-session -d -s tfsc 'cd $HOME/tfsc/ && $HOME/tfsc/tfs_v0.22.1_fffda3e_devnet -m'
+       rest37523563=$((rest37523563 + 1))
+        printf "sleep " 
+        printf $rest37523563
+        for((sec=0; sec<10; sec++))
+        do
+                printf "."
+                sleep 1
+        done
+        printf "\n"
+        
+done
