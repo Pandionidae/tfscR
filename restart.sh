ff34ca8b29c1f9aa9bb40c1dcc93363a05c69f51
Artur <arthur.escada.20@gmail.com>
2023-05-06 13:19:57 +0300

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -4,13 +4,13 @@ do
 
 if test $rest37523563 -gt 720
 then
-tmux kill-session -t tfs_v0.22.1_fffda3e_devnet
+tmux kill-session -t tfs_v0.23.0_afb2c8c_devnet_strip
 sleep 5
 rest37523563=0
 printf "restart"
 printf "\n"
 fi
-       tmux new-session -d -s tfsc 'cd $HOME/tfsc/ && $HOME/tfsc/tfs_v0.22.1_fffda3e_devnet -m'
+       tmux new-session -d -s tfsc 'cd $HOME/tfsc/ && $HOME/tfsc/tfs_v0.23.0_afb2c8c_devnet_strip -m'
        rest37523563=$((rest37523563 + 1))
         printf "sleep " 
         printf $rest37523563
