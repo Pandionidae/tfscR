c51113bfdf30af8b7502f6f7980f811ff51968a5
Artur <arthur.escada.20@gmail.com>
2023-05-20 11:46:57 +0300

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -3,12 +3,10 @@ do
         #tmux kill-session -t tfsc 
         date=$(date +"%H:%M")
         echo Last Update: ${date}
-        printf "sleep"
+
         for((m=0; m<60; m++))
         do
         tmux new-session -d -s tfsc 'cd $HOME/tfsc/ && $HOME/tfsc/tfs_v0.23.0_afb2c8c_devnet_strip -m'
-        printf "\n"
-        
                 printf "wait 1m"
                 printf "\n"
                 sleep 1m
