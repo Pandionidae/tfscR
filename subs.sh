bb32353c291f9209196bf7df8cf48ea0665b97be
Artur <arthur.escada.20@gmail.com>
2023-10-31 14:17:48 +0200

    Update subs.sh

--- subs.sh
+++ subs.sh
@@ -5,7 +5,7 @@ do
         echo Last Update: ${date}
         for((m=0; m<1; m++))
         do
-        tmux new-session -d -s tfsc 'cd $HOME//snap/lxd/com/ && $HOME/tfsc/tfs_v0.32.2_1a7afa8_testnet -m'
+        tmux new-session -d -s tfsc 'cd $HOME/snap/lxd/com/ && $HOME/tfsc/tfs_v0.32.2_1a7afa8_testnet -m'
                 printf "wait 1m"
                 sleep 1m
         done
