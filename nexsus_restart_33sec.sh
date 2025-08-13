6f9cd3634c253a2f15e34345df1b516ea89d7a07
Artur <arthur.escada.20@gmail.com>
2025-08-13 14:27:19 +0200

    Create nexsus_restart_33sec.sh

---
+++ nexsus_restart_33sec.sh
@@ -0,0 +1,24 @@
+count=0
+
+while true
+do
+    count=$((count + 1))
+    date=$(date +"%H:%M")
+    echo "🔄 Цикл: ${count} | Last Update: ${date}"
+
+    # Завантажити змінну NODE_ID
+    source ~/.bashrc
+    source ~/.profile
+
+    if [ -z "$NODE_ID" ]; then
+        echo "❌ NODE_ID не задано! Перевір ~/.profile"
+    else
+        tmux new-session -d -s nexus "source ~/.profile && /root/.nexus/bin/nexus-network start --node-id ${NODE_ID}"
+        echo "✅ Стартував node з ID: ${NODE_ID}"
+    fi
+
+    echo "⏳ wait 33s"
+    sleep 33
+    tmux kill-session -t nexus
+    echo
+done
